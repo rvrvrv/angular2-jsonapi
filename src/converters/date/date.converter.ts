b2fc1c2b6ddba08ab77502fa920f6c324621bd24
Nikolay Manolov <nikolay.manolov@asteasolutions.com>
2018-12-08 19:00:40 +0200

    Don't convert undefined dates

--- src/converters/date/date.converter.ts
+++ src/converters/date/date.converter.ts
@@ -3,10 +3,16 @@ import { PropertyConverter } from '../../interfaces/property-converter.interface
 
 export class DateConverter implements PropertyConverter {
   mask(value: any) {
+    if (value === undefined || value === null) {
+      return undefined;
+    }
     return parse(value);
   }
 
   unmask(value: any) {
+    if (value === undefined || value === null) {
+      return value;
+    }
     return format(value, 'YYYY-MM-DDTHH:mm:ssZ');
   }
 }
